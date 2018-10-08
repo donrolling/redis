5e0eae7ec886e05a189aa3533a01769a27bc6410
Don <donald.rolling@pcg.com>
2018-10-07 21:26:06 -0500

    first commit

---
+++ redis_console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+using System.Threading; 
+using StackExchange.Redis; 
+
+namespace redis_console {
+    class Program {
+		private const string mykey = "mykey";
+		private const string databaseConnectionUrl = "localhost";
+		private static ConnectionMultiplexer _redis; 
+
+        static void Main(string[] args) {
+            _redis = ConnectionMultiplexer.Connect(databaseConnectionUrl); 
+            entry(); 
+        }
+
+        private static void entry() {
+            try {
+                var db = _redis.GetDatabase(); 
+                db.StringSet(mykey, "Hello World"); 
+                var value = db.StringGet(mykey); 
+                Console.WriteLine(value); 
+                Thread.Sleep(10000); 
+                entry(); 
+            }catch(Exception ex) {
+                Console.WriteLine($"error: { ex.Message }");
+            }
+        }
+    }
+}
