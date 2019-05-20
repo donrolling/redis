d04e180809c0408b81efba4e6b14607035df6eeb
Don Rolling <donrolling@gmail.com>
2019-05-20 17:37:13 -0400

    Adding more items, better illustrating that everything comes from the cache

--- redis_console/Program.cs
+++ redis_console/Program.cs
@@ -1,29 +1,119 @@
-﻿using System; 
-using System.Threading; 
-using StackExchange.Redis; 
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using StackExchange.Redis;
 
-namespace redis_console {
-    class Program {
-		private const string mykey = "mykey";
-		private const string databaseConnectionUrl = "localhost";
-		private static ConnectionMultiplexer _redis; 
+namespace redis_console
+{
+    class Program
+    {
+        private const string databaseConnectionUrl = "localhost";
+        private static ConnectionMultiplexer _redis;
+        private static IDatabase _cache;
+        private static Random rnd = new Random(Guid.NewGuid().GetHashCode());
 
-        static void Main(string[] args) {
-            _redis = ConnectionMultiplexer.Connect(databaseConnectionUrl); 
-            entry(); 
+        private static Dictionary<string, string> _xs = new Dictionary<string, string> {
+            {"a", "Only don't tell me you're innocent. Because it insults my intelligence and makes me very angry."},
+            {"b", "I know it was you, Fredo. You broke my heart. You broke my heart!"},
+            {"c", "Michael, you never told me you knew Johnny Fontane! Never hate your enemies."},
+            {"d", "It affects your judgment. This one time, this one time I'll let you ask me about my affairs."},
+            {"e", "Is that why you slapped my brother around in public?"},
+            {"f", "You can act like a man! That's my family Kay, that's not me. "},
+            {"g", "My father is no different than any powerful man, any man with power, like a president or senator."},
+            {"h", "We're both part of the same hypocrisy, senator, but never think it applies to my family."},
+            {"i", "I'm your older brother, Mike, and I was stepped over!"},
+        };
+
+        static void Main(string[] args)
+        {
+            setup();
+            writeToCache();
+            while (true)
+            {
+                readFromCache();
+                Thread.Sleep(3000);
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)
+                {
+                    Console.WriteLine("\r\n");
+                    Console.WriteLine("Sleep with the fishes!");
+                    Thread.Sleep(3000);
+                    break;
+                }
+            }
         }
 
-        private static void entry() {
-            try {
-                var db = _redis.GetDatabase(); 
-                db.StringSet(mykey, "Hello World"); 
-                var value = db.StringGet(mykey); 
-                Console.WriteLine(value); 
-                Thread.Sleep(10000); 
-                entry(); 
-            }catch(Exception ex) {
+        private static string getLetter(int value)
+        {
+            return ((char)('a' + value)).ToString();
+        }
+
+        private static void setup()
+        {
+            Console.WriteLine("\r\n");
+            Console.WriteLine("Setup...");
+            _redis = ConnectionMultiplexer.Connect(databaseConnectionUrl);
+            _cache = _redis.GetDatabase();
+        }
+
+
+        private static void readFromCache()
+        {
+            Console.WriteLine("\r\n");
+            Console.WriteLine("----Press escape to exit----");
+            Console.WriteLine("\r\n");
+            Console.WriteLine("Reading from cache...");
+            try
+            {
+                var count = _xs.Count;
+                var some = count / 3;
+
+                var listNumbers = new List<int>();
+                int number = 0;
+                for (int i = 0; i < some; i++)
+                {
+                    do
+                    {
+                        number = rnd.Next(0, count);
+                    } while (listNumbers.Contains(number));
+                    listNumbers.Add(number);
+                    var letter = getLetter(number);
+                    var x = get(letter);
+                    Console.WriteLine($"{ letter }: { x }");
+                }
+            }
+            catch (Exception ex)
+            {
                 Console.WriteLine($"error: { ex.Message }");
             }
+        }
+
+        private static void writeToCache()
+        {
+            Console.WriteLine("\r\n");
+            Console.WriteLine("Writing to cache...");
+            try
+            {
+                foreach (var x in _xs)
+                {
+                    Console.WriteLine($"{ x.Key }: { x.Value }");
+                    set(x.Key, x.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"error: { ex.Message }");
+            }
+        }
+
+        private static void set(string key, string value)
+        {
+            _cache.StringSet(key, value);
+        }
+
+        private static string get(string key)
+        {
+            return _cache.StringGet(key);
         }
     }
 }
