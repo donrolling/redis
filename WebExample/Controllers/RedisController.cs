c36b7281d679ac94c8f3642944dfc2b5f6ebb7db
Rolling, Don <drolling@intrepiddirect.com>
2019-07-19 11:17:16 -0500

    add Redis Controller

---
+++ WebExample/Controllers/RedisController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : Controller
+    {
+        [HttpGet("{key}")]
+        public ActionResult<string> Get(string key)
+        {
+            return "";
+        }
+
+        [HttpPut("{key}")]
+        public ActionResult<bool> Put(string key, [FromBody] object value)
+        {
+            return true;
+        }
+    }
+}
